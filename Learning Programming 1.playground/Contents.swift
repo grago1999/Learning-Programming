475475c34d5c6c0175e63bd1563d80afbbf43ae2
grago1999 <grago1999@gmail.com>
2016-02-28 19:26:43 -0500

    1.0.0

---
+++ Learning Programming 1.playground/Contents.swift
@@ -0,0 +1,46 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+// Day 1: Basics
+
+
+// Commenting
+
+// This is a comment!
+
+/*
+This is an even longer comment!
+*/
+
+
+
+// Data Types
+
+let anInt = 7 // Static Data Type
+var anotherInt = 7 // Variable Data Type
+
+var myInt:Int = 7 // Int
+var myFloat:Float = 7.0 // Float
+var myDouble:Double = 7.0 // Double
+
+var myCharacter:Character = "h" // Character
+var myString:String = "This is my string!" // String
+var myBool:Bool = true // Boolean
+
+
+
+// Renaming Data Types
+
+typealias SpecialInteger = Int
+
+var mySpecialInteger:SpecialInteger = 0
+
+
+
+// Printing to Console
+
+print("Hello!")
+print(7)
+let floatToPrint = 7.0
+print(floatToPrint)
